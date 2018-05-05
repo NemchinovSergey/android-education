099616c0984e824017478dd75c0fa748cbceb411
Sergey Nemchinov <googoodemon@gmail.com>
2018-05-05 14:05:05 +0700

    added price calculation

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+
+    private int count;
+    private int price = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +25,16 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(++count);
+        displayPrice(count * price);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
